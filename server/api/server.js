3cde0402c42189a1faf6279122fd111231c2cbc3
Paulo Henrique Assis <pieit@live.com>
2021-11-15 23:21:57 -0300

    .

--- server/api/server.js
+++ server/api/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 
 const dbo = require('./models/connection');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   dbo.connectToServer((err) => {
