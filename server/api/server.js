f4dab5ad958ddc2a0c5642e3420607262530ab83
Paulo Henrique Assis <pieit@live.com>
2021-11-16 00:29:24 -0300

    .

--- server/api/server.js
+++ server/api/server.js
@@ -1,8 +1,17 @@
+const express = require('express');
 const app = require('./app');
+const path = require('path');
 
 const dbo = require('./models/connection');
 
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join('build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   dbo.connectToServer((err) => {
